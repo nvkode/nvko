020c5f585d32f2eb0747e7593ad61940a88766e2
Mykyta Melnyk <liswelus@gmail.com>
2023-02-26 09:56:22 +0100

    Moved phpstan into tools directory

--- tools/phpstan/object-manager.php
+++ tools/phpstan/object-manager.php
@@ -1,11 +1,15 @@
 <?php
 
+/**
+ * Loader for Doctrine Manager in PHPStan.
+ */
+
+require __DIR__ . '/../../vendor/autoload.php';
+
 use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/../vendor/autoload.php';
-
-(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+(new Dotenv())->bootEnv(__DIR__ . '/../../.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 
