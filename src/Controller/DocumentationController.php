052cac91f4acde134285c4de7585c87506f654df
Mykyta Melnyk <liswelus@gmail.com>
2023-02-28 11:12:47 +0100

    Parse nvdoc documentation in Twig

--- src/Controller/DocumentationController.php
+++ src/Controller/DocumentationController.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Exception;
+use Nvkode\Nvdoc\Nvdoc;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,22 +22,6 @@ use Symfony\Component\Routing\Annotation\Route;
 )]
 class DocumentationController extends AbstractController
 {
-
-    /**
-     * @var string $docsPath
-     */
-    private string $docsPath;
-
-
-    /**
-     * @param string $docsPath
-     */
-    public function __construct(
-        string $docsPath
-    ) {
-        $this->docsPath = $docsPath;
-
-    }
 
 
     /**
@@ -52,132 +36,29 @@ class DocumentationController extends AbstractController
     )]
     public function read(Request $request): Response
     {
-        $data = $this->parseDocumentation();
+        $projectDir = $this->getParameter('kernel.project_dir');
+        $data       = (new Nvdoc($projectDir))->getFilesInformation(sprintf("%s/%s", $projectDir, 'src'));
+
+        $currentNamespace = $request->get('namespace');
+        $namespaceDoc     = null;
+
+        if ($currentNamespace !== null
+            && trim($currentNamespace) !== ''
+            && array_key_exists($currentNamespace, $data) === true
+        ) {
+            $namespaceDoc = $data[$currentNamespace];
+        }
 
         return $this->render(
             '_docs/read.html.twig',
             [
-                'data' => $data,
-                'file' => $this->convertDocumentation($data, $request->get('hash')),
+                'data'             => $data,
+                'namespaceDoc'     => $namespaceDoc,
+                'currentNamespace' => $currentNamespace,
             ]
         );
 
     }//end index()
-
-
-    /**
-     * @param array<string, mixed> $property
-     *
-     * @return array<string, mixed>
-     */
-    private function getPropertyInformation(array $property): array
-    {
-        $docblock = $property['docblock'];
-        $tags     = $docblock['tag'];
-        $type     = null;
-
-        if (empty($tags) === false) {
-            if (count($tags) > 1) {
-                foreach ($tags as $tag) {
-                    if (array_key_exists('type', $tag) === true) {
-                        $type = $tag['type'];
-                        break;
-                    }
-                }
-            } else if (array_key_exists('@attributes', $tags) === true
-                && array_key_exists('type', $tags['@attributes']) === true
-            ) {
-                $type = $docblock['tag']['@attributes']['type'];
-            }
-        }
-
-        return [
-            'visibility'       => $property['@attributes']['visibility'],
-            'name'             => $property['name'],
-            'description'      => implode('.', $docblock['description']),
-            'long-description' => implode('.', $docblock['long-description']),
-            'type'             => $type,
-        ];
-
-    }//end getPropertyInformation()
-
-
-    /**
-     * @return array<string, mixed>
-     */
-    private function parseDocumentation(): array
-    {
-        try {
-            // Read XML file into string.
-            $xmlFile = file_get_contents($this->docsPath);
-
-            if ($xmlFile !== false) {
-                // Convert xml string into an object.
-                $xmlElement = simplexml_load_string($xmlFile);
-
-                // Convert xmlElement to JSON.
-                $xmlJSON = json_encode($xmlElement);
-
-                if ($xmlJSON !== false) {
-                    // Convert into associative array.
-                    $data = json_decode($xmlJSON, true);
-
-                    if (is_array($data) === true) {
-                        return $data;
-                    }
-                }
-            }//end if
-        } catch (Exception) {
-            // On error docs will be empty.
-        }
-
-        return [];
-
-    }//end parseDocumentation()
-
-
-    /**
-     * @param array<string, mixed> $data
-     * @param string|null          $fileHash
-     *
-     * @return array<string, mixed>|null
-     */
-    private function convertDocumentation(array $data, ?string $fileHash): array|null
-    {
-        $file = null;
-
-        if (empty($data) === false && $fileHash !== null) {
-            foreach ($data['file'] as $file) {
-                if ($file['@attributes']['hash'] === $fileHash) {
-                    $parsedProperties = [];
-                    $properties       = [];
-
-                    if (array_key_exists('property', $file['class']) === true) {
-                        $properties = $file['class']['property'];
-                    }
-
-                    if (array_key_exists('name', $properties) === true) {
-                        $parsedProperties[] = $this->getPropertyInformation($properties);
-                    } else if (empty($properties) !== true) {
-                        foreach ($properties as $property) {
-                            $parsedProperties[] = $this->getPropertyInformation($property);
-                        }
-                    }
-
-                    $file = [
-                        'path'       => $file['@attributes']['path'],
-                        'name'       => $file['class']['name'],
-                        'extends'    => $file['class']['extends'],
-                        'properties' => $parsedProperties,
-                    ];
-                    break;
-                }
-            }
-        }
-
-        return $file;
-
-    }
 
 
 }//end class
